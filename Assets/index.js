38263827e8cc7f2c909a009ccb6163ffe1b4a678
Andrew <andrewg87@gmail.com>
2022-11-21 12:29:56 -0500

    set up the .gitignore, package.json, and got inquirer running. also created one question in index.js to test it out

---
+++ Assets/index.js
@@ -0,0 +1,24 @@
+// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown');
+
+// TODO: Create an array of questions for user input
+const questions = [
+  {
+    name: 'projectName',
+    message: 'What is the name of your project?',
+    type: 'input'
+  }
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {
+  return inquirer.prompt(questions);
+}
+
+// Function call to initialize app
+init();
