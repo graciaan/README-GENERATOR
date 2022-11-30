911a486b7dd1f86b512d028c00740979e7d35e97
Andrew <andrewg87@gmail.com>
2022-11-30 11:22:49 -0500

    commiting recent changes. generatemarkdown.js still not completed, will require changes before application works

--- Assets/index.js
+++ Assets/index.js
@@ -1,9 +1,10 @@
-// TODO: Include packages needed for this application
+//List of required packages and files
 const inquirer = require('inquirer');
 const fs = require('fs');
 const generateMarkdown = require('./utils/generateMarkdown');
 
-// TODO: Create an array of questions for user input
+
+//These are the question arrays needed to complete the info for the README
 const questions = [
   {
     name: 'projectName',
@@ -19,7 +20,7 @@ const questions = [
   
   {
     name: 'projectInstructions',
-    message: 'Please provide instructions on how to use the application',
+    message: 'Please provide instructions on how to use and install the application',
     type: 'text'
   },
 
@@ -39,16 +40,29 @@ const questions = [
     name: 'projectScreenshot',
     message: 'Please include a link to a screenshot of the project',
     type: 'text'
+  },
+  
+  {
+    name: 'projectLicense',
+    message: 'Please select the licensing used for this project',
+    choices: ['Apache License 2.0', 'MIT', 'Mozilla Public License 2.0', 'Creative Commons Zero 1.0', 'No License'],   
+    type: 'list'
   }
 ];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
-
-// TODO: Create a function to initialize app
+//This function runs the app and also creates the README file
 function init() {
-  return inquirer.prompt(questions);
-}
+  inquirer.prompt(questions).then(function(userInput) {
+    const markdown = generateMarkdown(userInput)
+    fs.writeFile('README.md', markdown, error => {
+      if(error) {
+        console.log(error);
+      } else {
+        console.log("README successfully created!")
+      }
+    })
+  })
+};
 
 // Function call to initialize app
 init();
--- Assets/utils/generateMarkdown.js
+++ Assets/utils/generateMarkdown.js
@@ -1,17 +1,71 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
-
+function renderLicenseBadge(license) {
+  if(`${data.projectLicense}` == 'Apache License 2.0'){
+    LicenseBadge = 'https://img.shields.io/badge/License-Apache_2.0-blue.svg'
+  } else if (`${data.projectLicense}` == 'MIT'){
+    LicenseBadge = 'https://img.shields.io/badge/License-MIT-yellow.svg'
+  } else if (`${data.projectLicense}` == 'Mozilla Public License 2.0'){
+    LicenseBadge = 'https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg'
+  } else if (`${data.projectLicense}` == 'Creative Commons Zero 1.0'){
+    LicenseBadge = 'https://licensebuttons.net/l/zero/1.0/80x15.png'
+  } else {
+    LicenseBadge = ' '
+  }
+}
+renderLicenseBadge();
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
-
+function renderLicenseLink(license) {  
+  if (`${data.projectLicense}` == 'Apache License 2.0'){
+  licenseLink = 'https://opensource.org/licenses/Apache-2.0'
+} else if (`${data.projectLicense}` == 'MIT'){
+  licenseLink = 'https://opensource.org/licenses/MIT'
+} else if (`${data.projectLicense}` == 'Mozilla Public License 2.0'){
+  licenseLink = 'https://opensource.org/licenses/MPL-2.0'
+} else if (`${data.projectLicense}` == 'Creative Commons Zero 1.0'){
+  licenseLink = 'http://creativecommons.org/publicdomain/zero/1.0/'
+} else {
+  licenseLink = ' '
+}}
+renderLicenseLink();
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (`${data.projectLicense}` == 'No License'){
+    return " "
+  } else {return `
+  ## License
+  [![License: ${data.projectLicense}](${data.licenseBadge})](${data.licenseLink})`
+  } 
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
+  if(`${data.projectLicense}` == 'Apache License 2.0'){
+    LicenseBadge = 'https://img.shields.io/badge/License-Apache_2.0-blue.svg'
+  } else if (`${data.projectLicense}` == 'MIT'){
+    LicenseBadge = 'https://img.shields.io/badge/License-MIT-yellow.svg'
+  } else if (`${data.projectLicense}` == 'Mozilla Public License 2.0'){
+    LicenseBadge = 'https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg'
+  } else if (`${data.projectLicense}` == 'Creative Commons Zero 1.0'){
+    LicenseBadge = 'https://licensebuttons.net/l/zero/1.0/80x15.png'
+  } else {
+    LicenseBadge = ' '
+  };
+
+  if (`${data.projectLicense}` == 'Apache License 2.0'){
+    licenseLink = 'https://opensource.org/licenses/Apache-2.0'
+  } else if (`${data.projectLicense}` == 'MIT'){
+    licenseLink = 'https://opensource.org/licenses/MIT'
+  } else if (`${data.projectLicense}` == 'Mozilla Public License 2.0'){
+    licenseLink = 'https://opensource.org/licenses/MPL-2.0'
+  } else if (`${data.projectLicense}` == 'Creative Commons Zero 1.0'){
+    licenseLink = 'http://creativecommons.org/publicdomain/zero/1.0/'
+  } else {
+    licenseLink = ' '
+  };
+
   return `# ${data.projectName}
   ## Description
   ${data.projectDetails}
@@ -27,7 +81,14 @@ function generateMarkdown(data) {
 
   ## Project Screenshot
   ![Screenshot](${data.projectScreenshot})
-`;
+
+  ## License
+  [![License: ${data.projectLicense}](${data.licenseBadge})](${data.licenseLink})`
+
+
+  // ## License
+  // [![License: ${data.projectLicense}](${renderLicenseBadge(data.license)})](${renderLicenseLink(data.license)})
+
 }
 
 module.exports = generateMarkdown;
