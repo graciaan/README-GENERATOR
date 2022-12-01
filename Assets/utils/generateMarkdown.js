d89ba946162792bf164674629a746f5798f9a78c
Andrew <andrewg87@gmail.com>
2022-12-01 10:42:24 -0500

    made changes that has the markdown file working. had to fix capitalization of some variables

--- Assets/utils/generateMarkdown.js
+++ Assets/utils/generateMarkdown.js
@@ -1,57 +1,16 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {
-  if(`${data.projectLicense}` == 'Apache License 2.0'){
-    LicenseBadge = 'https://img.shields.io/badge/License-Apache_2.0-blue.svg'
-  } else if (`${data.projectLicense}` == 'MIT'){
-    LicenseBadge = 'https://img.shields.io/badge/License-MIT-yellow.svg'
-  } else if (`${data.projectLicense}` == 'Mozilla Public License 2.0'){
-    LicenseBadge = 'https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg'
-  } else if (`${data.projectLicense}` == 'Creative Commons Zero 1.0'){
-    LicenseBadge = 'https://licensebuttons.net/l/zero/1.0/80x15.png'
-  } else {
-    LicenseBadge = ' '
-  }
-}
-renderLicenseBadge();
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {  
-  if (`${data.projectLicense}` == 'Apache License 2.0'){
-  licenseLink = 'https://opensource.org/licenses/Apache-2.0'
-} else if (`${data.projectLicense}` == 'MIT'){
-  licenseLink = 'https://opensource.org/licenses/MIT'
-} else if (`${data.projectLicense}` == 'Mozilla Public License 2.0'){
-  licenseLink = 'https://opensource.org/licenses/MPL-2.0'
-} else if (`${data.projectLicense}` == 'Creative Commons Zero 1.0'){
-  licenseLink = 'http://creativecommons.org/publicdomain/zero/1.0/'
-} else {
-  licenseLink = ' '
-}}
-renderLicenseLink();
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {
-  if (`${data.projectLicense}` == 'No License'){
-    return " "
-  } else {return `
-  ## License
-  [![License: ${data.projectLicense}](${data.licenseBadge})](${data.licenseLink})`
-  } 
-}
+//created one function that generates the markdown and also generates the badge and license link.
 
-// TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   if(`${data.projectLicense}` == 'Apache License 2.0'){
-    LicenseBadge = 'https://img.shields.io/badge/License-Apache_2.0-blue.svg'
+    licenseBadge = 'https://img.shields.io/badge/License-Apache_2.0-blue.svg'
   } else if (`${data.projectLicense}` == 'MIT'){
-    LicenseBadge = 'https://img.shields.io/badge/License-MIT-yellow.svg'
+    licenseBadge = 'https://img.shields.io/badge/License-MIT-yellow.svg'
   } else if (`${data.projectLicense}` == 'Mozilla Public License 2.0'){
-    LicenseBadge = 'https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg'
+    licenseBadge = 'https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg'
   } else if (`${data.projectLicense}` == 'Creative Commons Zero 1.0'){
-    LicenseBadge = 'https://licensebuttons.net/l/zero/1.0/80x15.png'
+    licenseBadge = 'https://licensebuttons.net/l/zero/1.0/80x15.png'
   } else {
-    LicenseBadge = ' '
+    licenseBadge = ' '
   };
 
   if (`${data.projectLicense}` == 'Apache License 2.0'){
@@ -83,11 +42,7 @@ function generateMarkdown(data) {
   ![Screenshot](${data.projectScreenshot})
 
   ## License
-  [![License: ${data.projectLicense}](${data.licenseBadge})](${data.licenseLink})`
-
-
-  // ## License
-  // [![License: ${data.projectLicense}](${renderLicenseBadge(data.license)})](${renderLicenseLink(data.license)})
+  [![License: ${data.projectLicense}](${licenseBadge})](${licenseLink})`
 
 }
 
